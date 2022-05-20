f353a58d6a577129ea47ee48648d8d4d2b8cb41c
jennerdulce <jennerdulce@gmail.com>
2022-05-20 09:45:33 -0700

    created work environment

---
+++ template-literals.js
@@ -0,0 +1,49 @@
+// Challenges
+// Exercise 1
+// Consider this variable:
+
+// var meaningOfLife = 42
+// Write the code that prints 'The answer to the meaning of life is 42'.
+
+// Exercise 2
+// Consider this code:
+
+// var zooAnimals = ['giraffe', 'fennec fox', 'elephant']
+
+// zooAnimals.forEach(animal =>  console.log('The zoo has a' + animal))
+// Refactor the .forEach method to use string interpolation.
+
+// Exercise 3
+// Consider this variable:
+
+// var burger = {
+//     cheese: 'cheddar',
+//     patty: 'black bean',
+//     onions: false,
+// }
+// Write the code that prints 'black bean burger with cheddar' to the screen.
+
+// STRETCH
+
+// Given these 2 burger objects:
+
+// var beanBurger = {
+//     cheese: 'cheddar',
+//     patty: 'black bean',
+//     onions: false,
+// }
+
+// var turkeyBurger = {
+//     cheese: 'swiss',
+//     patty: 'turkey',
+//     onions: true,
+// }
+// Write a function takes a burger object and returns a descriptive string.
+
+// For example:
+
+// describeBurger(beanBurger)
+// --> "black bean burger with cheddar and no onions"
+
+// describeBurger(turkeyBurger)
+// --> "turkey burger with swiss and onions"
---
+++ ternary.js
@@ -0,0 +1,3 @@
+// Challenges
+// Create a function that determines if a user is old enough to vote (age 18 or older).
+// Create a function that takes in an array of numbers and determines if the number is odd or even.
